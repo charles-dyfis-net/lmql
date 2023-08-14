ce92ce89b49b875750e60ae4f627b96a116df1e8
Charles Duffy <charles@dyfis.net>
2023-08-14 08:32:11 -0500

    Implement Nix flake packaging for LMQL

---
+++ flake.d/overrides.nix
@@ -0,0 +1,77 @@
+{ llamaDotCpp, poetry2nix }:
+let
+  # Make a fixed-output derivation with a file's contents; can be used to avoid making something depend on the entire
+  # lmql source tree when it only needs one file.
+  makeFOD = pkgs: smallSourceFile: pkgs.runCommand (builtins.baseNameOf smallSourceFile) {
+        outputHashMode = "flat";
+        outputHashAlgo = "sha256";
+        outputHash = builtins.hashFile "sha256" smallSourceFile;
+        inherit smallSourceFile;
+      } ''
+        rmdir -- "$out" ||:
+        cp -- "$smallSourceFile" "$out"
+      '';
+  # Some prebuilt operations we often need to do to make Python packages build
+
+  # The lazy version: Give up on building it from source altogether and use a binary
+  preferWheel = { name, final, prev, pkg }: pkg.override { preferWheel = true; };
+
+  # Add extra inputs needed to build from source; often things like setuptools or hatchling not included upstream
+  addBuildInputs = extraBuildInputs: { name, final, prev, pkg }:
+    pkg.overridePythonAttrs (old: {
+      buildInputs = (old.buildInputs or []) ++ (builtins.map (dep: if builtins.isString dep then builtins.getAttr dep final else dep) extraBuildInputs);
+    });
+
+  # Rust packages need extra build-time dependencies; and if the upstream repo didn't package a Cargo.lock file we need to add one for them
+  asRustBuild = { name, final, prev, pkg }:
+    let
+      lockFilePath = ./cargo-deps/. + "/${pkg.pname}-${pkg.version}-Cargo.lock";
+      lockFile = makeFOD prev.pkgs lockFilePath;
+      haveLockFileOverride = builtins.pathExists lockFilePath;
+    in pkg.overridePythonAttrs (old: {
+      buildInputs = (old.buildInputs or []) ++ [ final.setuptools final.setuptools-rust final.pkgs.iconv ] ++
+        final.pkgs.lib.optional final.pkgs.stdenv.isDarwin final.pkgs.darwin.apple_sdk.frameworks.Security;
+      nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ final.pkgs.cargo final.pkgs.rustc final.pkgs.rustPlatform.cargoSetupHook ];
+    } // (if haveLockFileOverride then {
+      cargoDeps = final.pkgs.rustPlatform.importCargoLock { inherit lockFile; };
+      prePatch = ''
+        cp -- ${lockFile} ./Cargo.lock
+        ${old.prePatch or ""}
+      '';
+    } else {}));
+
+  # Use the libllama.dylib or libllama.so from llamaDotCpp instead of letting the package build its own
+  llamaCppUseLlamaBuild = { name, final, prev, pkg }: pkg.overridePythonAttrs (old: {
+    prePatch = (old.prePatch or "") + "\n" + ''
+      ${final.pkgs.gnused}/bin/sed -i -e 's@from skbuild import setup@from setuptools import setup@' setup.py
+    '';
+    postInstall = ''
+      oldWD=$PWD
+      ln -s -- ${llamaDotCpp.packages.${final.pkgs.system}.default}/lib/libllama.* "$out"/lib/*/site-packages/llama_cpp/ || exit
+      cd "$oldWD" || exit
+    '';
+  });
+
+  composeOps = opLeft: opRight:
+    { name, final, prev, pkg } @ argsIn:
+      let firstResult = (opLeft argsIn);
+      in opRight { inherit name final; prev = prev // { "${name}" = firstResult; }; pkg = firstResult; };
+
+  # Python eggs only record runtime dependencies, not build dependencies; so we record build deps that aren't autodetected here.
+  buildOps = {
+    accelerate           = addBuildInputs [ "filelock" "jinja2" "networkx" "setuptools" "sympy" ];
+    accessible-pygments  = addBuildInputs [ "setuptools" ];
+    llama-cpp-python     = composeOps (addBuildInputs [ "setuptools" ]) llamaCppUseLlamaBuild;
+    pandoc               = addBuildInputs [ "setuptools" ];
+    pydata-sphinx-theme  = preferWheel;
+    safetensors          = asRustBuild;
+    shibuya              = addBuildInputs [ "setuptools" ];
+    sphinx-book-theme    = preferWheel;
+    sphinx-theme-builder = addBuildInputs [ "filit-core" ];
+    tiktoken             = asRustBuild;
+    tokenizers           = asRustBuild;
+    torch                = addBuildInputs [ "filelock" "jinja2" "networkx" "sympy" ];    
+    urllib3              = addBuildInputs [ "hatchling" ];
+  };
+  buildOpsOverlay = (final: prev: builtins.mapAttrs (package: op: (op { inherit final prev; name = package; pkg = builtins.getAttr package prev; })) buildOps);
+in poetry2nix.overrides.withDefaults buildOpsOverlay
---
+++ flake.nix
@@ -0,0 +1,157 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+    poetry2nix.url = "github:nix-community/poetry2nix";
+    poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
+    llamaDotCpp.url = "github:ggerganov/llama.cpp";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils, llamaDotCpp, poetry2nix }: let
+    llamaDotCppFlake = llamaDotCpp;
+    poetry2nixFlake = poetry2nix;
+    nonSystemSpecificOutputs = {
+      overlays = {
+        noSentencePieceCustomMallocOnDarwin = (final: prev: {
+          sentencepiece = if prev.stdenv.isDarwin then prev.sentencepiece.override { withGPerfTools = false; } else prev.sentencepiece;
+        });
+      };
+    };
+  in nonSystemSpecificOutputs // flake-utils.lib.eachSystem [ "aarch64-darwin" "x86_64-linux" ] (system: let
+    version =
+      if self.sourceInfo ? "rev"
+      then "${self.sourceInfo.lastModifiedDate}-${self.sourceInfo.shortRev}"
+      else "dirty";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [
+        nonSystemSpecificOutputs.overlays.noSentencePieceCustomMallocOnDarwin
+        poetry2nixFlake.overlay
+      ];
+    };
+    poetryEnv = pkgs.poetry2nix.mkPoetryEnv {
+      python = pkgs.python310;
+      projectDir = "${self}/flake.d";
+      # projectDir = "${self}/src";
+      overrides = import ./flake.d/overrides.nix { inherit (pkgs) poetry2nix; inherit llamaDotCpp; };
+      editablePackageSources = {
+        lmql = self;
+      };
+    };
+  in rec {
+    legacyPackages = pkgs;
+    packages = {
+      llamaDotCpp = llamaDotCppFlake.packages.${system}.default;
+      python = poetryEnv;
+      lmql-docs = pkgs.runCommand "lmql-docs" {
+        python = pkgs.python310.withPackages (p: [p.myst-parser p.pydata-sphinx-theme p.sphinx-book-theme p.nbsphinx]);
+        docSource = ./docs/source;
+      } ''
+        PATH=${pkgs.pandoc}/bin:$PATH ${poetryEnv}/bin/sphinx-build "$docSource" "$out"
+      '';
+      playground = pkgs.mkYarnPackage rec {
+        pname = "lmql-playground-live";
+        inherit version;
+        src = ./src/lmql/ui/live;
+        yarnLock = "${src}/yarn.lock";
+        yarnNix = "${src}/yarn.nix";
+        packageJSON = "${src}/package.json";
+        dontStrip = true;
+
+        buildPhase = ''
+          true
+        '';
+
+        distPhase = ''
+          # We need a Python interpreter with all the dependencies
+          mkdir -p -- $out/bin $out/libexec
+          ln -s ${packages.python}/bin/python "$out/bin/python"
+          ln -s ${pkgs.nodejs}/bin/node "$out/bin/node"
+
+          ln -s ${pkgs.writeShellScript "lmql-live-run" ''
+            bindir=''${BASH_SOURCE%/*}
+            : addr=''${addr:=127.0.0.1} port=''${port:=3000}
+            cd "$bindir/../libexec/liveserve/deps/liveserve" || exit
+            export PATH=$bindir:$PATH
+            export PYTHONPATH=${self}/src:$PYTHONPATH
+            export NODE_PATH=$out/libexec/node_modules
+            export PORT=$port
+            export content_dir=${packages.playground-static-content}/content
+            exec "$bindir/node" "live.js"
+          ''} "$out/bin/run"
+        '';
+
+        meta.mainProgram = "run";
+      };
+      # static content 
+      playground-static-content = pkgs.mkYarnPackage rec {
+        pname = "web";
+        inherit version;
+        src = ./src/lmql/ui/playground;
+        yarnLock = "${src}/yarn.lock";
+        yarnNix = "${src}/yarn.nix";
+        packageJSON = "${src}/package.json";
+        dontStrip = true;
+
+        patchPhase  = ''
+          find . -type d -name browser-build -exec rm -rf -- {} +
+          rm -f -- public/doc-snippets
+        '';
+
+        DISABLE_ESLINT_PLUGIN = "true";
+
+        buildPhase = ''
+          HOME=$(mktemp -d) yarn --offline build
+        '';
+
+        distPhase = ''
+          shopt -s extglob
+          mv "$out"/libexec/web/deps/web/build "$out/content"
+          rm -rf -- "$out"/!(content)
+
+          mkdir -p -- "$out/bin"
+          ln -s ${pkgs.writeShellScript "lmql-playground-run" ''
+            bindir=''${BASH_SOURCE%/*}
+            : addr=''${addr:=127.0.0.1} port=''${port:=3000}
+            echo "Starting web server on $addr:$port..." >&2
+            exec ${packages.python}/bin/python -m http.server --bind "$addr" --directory "$bindir/../content" "$port"
+          ''} "$out/bin/run" 
+        '';
+
+        meta.mainProgram = "run";
+      };
+      # TODO: Add at entrypoint to run a LMTP server
+      # TODO: Add an entrypoint to run a Python interpreter with LMQL and dependencies
+    };
+    devShells = let
+      jsDevPackages = [
+        pkgs.yarn
+        pkgs.yarn2nix
+      ];
+      pythonDevPackages = [
+        pkgs.pandoc # not python-specific, but used in building docs
+        pkgs.poetry
+        pkgs.poetry2nix.cli
+        (pkgs.python310.withPackages (p: [p.poetry-core]))
+      ];
+      runtimePackages = [
+        llamaDotCppFlake.packages.${system}.default
+        pkgs.nodejs
+      ];
+    in {
+      # python interpreter able to import lmql successfully
+      default = poetryEnv.env.overrideAttrs (oldAttrs: {
+        shellHook = ''
+          PS1='[lmql] '"$PS1"
+          lmql() { python -m lmql.cli "$@"; }
+        '';
+        PYTHONPATH = "${builtins.toString ./src}";
+        buildInputs = jsDevPackages ++ pythonDevPackages ++ runtimePackages;
+      });
+      # tools to run poetry and yarn2nix, and nothing else
+      minimal = pkgs.mkShell {
+        name = "minimal-dev-shell";
+        buildInputs = jsDevPackages ++ pythonDevPackages;
+      };
+    };
+  });
+}
